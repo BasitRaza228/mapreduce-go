d57dc4bb17fec8e4502439b386e80cdb4e0ea362
Syed Basit Raza <basitraza228@gmail.com>
2025-01-17 01:39:07 +0500

    Chore core setup

--- src/types/keyvalue.go
+++ src/types/keyvalue.go
@@ -1,12 +1,13 @@
 package types
 
+// represents Map of MapReduce Framework
 type KeyValue struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
 
-// signature for map function
+// signature for map functions
 type MapFunc func(filename string, contents string) []KeyValue
 
-// signature for reduce function
+// signature for reduce functions
 type ReduceFunc func(key string, values []string) string
